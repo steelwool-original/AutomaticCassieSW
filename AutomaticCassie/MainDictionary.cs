d0ee15b1af39ca57ee1fcccdb9baae5b7dcace45
domin <domin@DESKTOP-HF7UC41>
2025-03-05 20:26:49 +0100

    l

--- AutomaticCassie/MainDictionary.cs
+++ AutomaticCassie/MainDictionary.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using CommandSystem;
+using Exiled.API.Enums;
 using Exiled.API.Features;
+using Exiled.API.Features.Doors;
+using UnityEngine;
 
 
 namespace AutomaticCassie
@@ -320,15 +323,50 @@ namespace AutomaticCassie
             {
                 Cassie.MessageTranslated("pitch_0.4 .g2 .g1 .g5 .g2 .g3 pitch_0.7 jam_022_4 Warning pitch_0.85 class D termination jam_060_7 cancel . MTFunits .g5 secure all jam_036_5 class D personnel pitch_0.3 .g1 .g5 .g3 .g1", "<color=#FF0000>Ostrzeżenie</color>: Terminacja <color=#FF7700>klasy D</color> zostało anulowane. Zabezpieczyć cały personel <color=#FF7700>klasy D</color>.", isNoisy: false);
             }
-            else 
-            {
-                whatEvent = "CASSIE error";
-            }
 
             /// <summary>
             /// Metoda funkcji parametru classd.
             /// </summary>
 
+        }
+
+        public static void CodeCassie(string whatCode) 
+        {
+            if (whatCode == "pomarańczowy")
+            {
+                Cassie.MessageTranslated("bell_start pitch_0.5 .g2 pitch_0.9 .g1 pitch_0.9 jam_043_5 Warning .g1 .g3 .g4 Code jam_090_1 Orange has been jam_080_3 activated pitch_0.7 .g5 pitch_0.9 Class D jam_090_2 personnel have jam_080_3 started .g1 an insurgency pitch_0.7 .g5 pitch_0.6 .g5 pitch_0.9 jam_080_3 To all personnel proceed with standard security protocols pitch_1 bell_end pitch_0.1 .g3 pitch_1", "Uwaga . <color=#FFCC99>Kod Pomarańczowy</color> został aktywowany. Personel <color=#FFA550>Klasy D</color> Rozpoczął bunt Cały Personel ma za zadanie postępować według standardowych procedur kodu <color=#FFCC99>pomarańczowego</color>.", isNoisy: true);
+                CheckpointDoor.Get(DoorType.CheckpointEzHczA).Lock(time: 180, lockType: DoorLockType.Lockdown079);
+                CheckpointDoor.Get(DoorType.CheckpointEzHczB).Lock(time: 180, lockType: DoorLockType.Lockdown079);
+            }
+            else if (whatCode == "czarny")
+            {
+                Cassie.MessageTranslated("pitch_0.2 .g5 pitch_0.4 .g5 pitch_0.95 Foundation Personnel .g6 Code Black Has Been Initiated pitch_0.3 .g5 .g5 pitch_0.9 All Foundation Forces Please Evacuate Site Personnel .g1 .g1 On Surface Zone .g5 . .g5", "Uwaga Personelu Fundacji, <color=#737373>Kod Czarny</color> został uruchomiony. Wszystkie siły zbrojne Fundacji powinny pomóc ewakuawać Personel na Strefę Zewnętrzną.", isNoisy: true);
+            }
+            else if (whatCode == "zielony")
+            {
+                Cassie.MessageTranslated("pitch_0.2 .g5 pitch_0.9 .g5 .g5 pitch_0.95 Attention Foundation Personnel .g6 Code Green Has Been Initiated pitch_0.3 .g5 .g1 pitch_0.94 Group Of Interest Against Foundation HasEntered .g4 pitch_0.99 .g4 All Personnel Stay in safe area .g5 .g5 MTFunits proceed with standard procedures pitch_0.8 .g5 . .g5 pitch_1", "Uwaga Do Personelu Fundacji, <color=#2cbf1f>Kod Zielony</color> został aktywowany. Nieznana grupa wrogich jednostek wkroczyła do placówki. Cały Personel ma pozostać w bezpiecznym miejscu, <color=#ADD8E6>Mobilne Formacje Operacyjne</color> Stosować się do Standardowych Procedur", isNoisy: true);
+            }
+        }
+
+        public static void EnterCassie(string whatDivision) 
+        {
+            if (whatDivision == "nu7")
+            {
+                Cassie.MessageTranslated("bell_start pitch_0.80 Alert . .g1 Alert .g5 .g3 pitch_0.90 MTFunit Nu 7 jam_080_6 has entered the Facility through Gate jam_070_1 B . Any activity jam_050_2 against MTFunit .g3 Nu 7 will be .g1 jam_070_4 stopped by termination if needed .g3 .g4 . MTFunit .g2 Nu 7 will stay in .g1 Site 0 2 until this Facility will be safe .g4 .g5 .g6 bell_end", "<color=#FF0000>Ogłoszenie</color>: Jednostka <color=#ADD8E6>MTF</color> <color=#73ad71>Nu-7</color> wkroczyła do placówki przez bramę B. Wszystkie działania sprzeczne z protokołami jednostki <color=#ADD8E6>MTF</color> zostaną natychmiast zatrzymane, a w razie potrzeby, także przez terminację. Jednostka <color=#73ad71>Nu-7</color> będzie utrzymywać obecność w placówce, aż do momentu zapewnienia pełnego bezpieczeństwa obiektu.", isNoisy: false);
+            }
+            else if (whatDivision == "a1")
+            {
+                Cassie.MessageTranslated("pitch_0.3 .g1 .g2 .g5 pitch_0.99 Attention To All MTFunits .g6 In jam_005_4 pitch_0.87 Foundation . pitch_0.09 .g4 .g4 pitch_0.8 jam_044_2 High Personnel Of Foundation Team . Decide To .g1 .g3 .g1 Send . Alpha 1 .g6 Red Right Hand .g5 To Site 02 . pitch_0.1 .g4 .g5 .g2 .g3 pitch_0.7 . jam_005_7 Warning . pitch_1.4 .g2 .g2 .g4 pitch_0.8 jam_005_7 All . pitch_0.8 Survivors Are Advised To Cooperate With Alpha 1 pitch_1 .g4 .g6 pitch_0.87 If You Do Something Against Foundation Forces pitch_1.4 .g2 .g2 .g4 pitch_0.8 You Will Be Designated For jam_040_2 pitch_0.7 Neutralization pitch_0.09 .g4 .g4", "Uwaga do wszystkich Mobilych Formacji Operacyjnych w ośrodku, Wysoko postawiony personel zadecydował o wysłaniu MFO <color=#FF0000>Alpha-1</color> do Ośrodka-02. Cały personel zobowiązany jest do kooperowania z jednostką <color=#FF0000>Alpha-1</color> W przypadku działania przeciwko jednostce zostanie wydany <color=#FF0000>nakaz terminacji</color>.", isNoisy: false);
+            }
+            else if (whatDivision == "o1")
+            {
+                Cassie.MessageTranslated("bell_start pitch_0.80 Alert . .G1 Alert .g5 .g3 pitch_0.8 MTFunit Omega 1 has jam_080_6 entered the Facility through Gate jam_070_1 B . . . Command to all remaining personnel .g4 .g3 . .g1 execute orders from MTFunit Omega 1 or you will be arrested . .g3 .g4 . jam_060_4 Omega 1 will leave the facility after they reach their .g1 target pitch_0.1 .g3 .g4 .g6 bell_end", "<color=#FF0000>Uwaga</color>, <color=#FF0000>Uwaga</color>! Jednostka <color=#ADD8E6>MTF</color> Omega-1 wkroczyła do placówki przez Bramę B. Komenda do całego pozostałego personelu: Bezwzględnie podporządkować się poleceniom jednostki Omega-1, w przeciwnym razie grozi <color=#FF0000>aresztowanie</color>. Omega-1 opuści placówkę po osiągnięciu celu.", isNoisy: false);
+            }
+        }
+
+        public static void OnDetonationCentreCassie() 
+        {
+            Cassie.MessageTranslated("cassie_sl pitch_0.1 .g4 . .g4 pitch_0.9 Warning . . pitch_0.8 Warning . facility status is critical .g1 . detonation jam_044_2 center .g2  jam_044_2  decided that site 0 2 is designated .g6 for jam_044_2  detonation .g2 with Alpha Warhead . please evacuate immediately . MtfUnits evacuate to surface .g1 zone and secure elevators . jam_044_2  detonation sequence .g3 will be jam_044_2  initiated in 30 seconds . emergency jam_044_2  generators will be initiated in 3 . . 2 . . 1 . .G1 .G3 .G2", "<color=#FF0000>Uwaga</color> do całego personelu, Status <color=#ADD8E6>Placówki</color> jest <color=#FF0000>krytyczny</color> Centrum Detonacji zadecydowało o detonacji <color=#ADD8E6>Ośrodka-02</color>. Zadaniem całego personelu jest ewakuacja na strefę zewnętrzną wraz z Mobilnymi Formacjami Operacyjnymi. E-11 zabezpieczcie windy surface zone Sekwencja detonacji rozpocznie się za 30 sekund, Awaryjne generatory zostaną uruchomione za 3.. 2.. 1..");
         }
 
     }
@@ -495,6 +533,42 @@ namespace AutomaticCassie
                         codeword = arguments.At(1).ToLower();
 
                         CassieDeathsies.ClassDShit(codeword);
+                    }
+                    break;
+                case "kod":
+                    if (arguments.Count > 2)
+                    {
+                        response = "Too many arguments!";
+                        return false;
+                    }
+                    else if (arguments.Count < 2)
+                    {
+                        response = "No arguments!";
+                        return false;
+                    }
+                    else
+                    {
+                        string codeword;
+                        codeword = arguments.At(1).ToLower();
+
+                        CassieDeathsies.CodeCassie(codeword);
+                    }
+                    break;
+                case "enter":
+                case "facilitydetonate":
+                    if (arguments.Count < 1) 
+                    {
+                        response = "No arguments!";
+                        return false;   
+                    }
+                    else if(arguments.Count > 1) 
+                    {
+                        response = "Too many arguments!";
+                        return false;
+                    }
+                    else 
+                    {
+                        CassieDeathsies.OnDetonationCentreCassie();
                     }
                     break;
                 default:
